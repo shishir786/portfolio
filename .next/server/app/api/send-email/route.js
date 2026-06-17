3895697590a606c33141a0d9204dc2a310cdb8c1
Abdullah Shishir <112066110+shishir786@users.noreply.github.com>
2026-06-17 15:33:03 +0600

    update

--- .next/server/app/api/send-email/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/send-email/route.js")
-R.c("server/chunks/[externals]__0wmb6rc._.js")
-R.c("server/chunks/[root-of-the-server]__0ui7sg2._.js")
-R.c("server/chunks/[root-of-the-server]__0_.xpdm._.js")
-R.c("server/chunks/_next-internal_server_app_api_send-email_route_actions_05jukv..js")
-R.m(96587)
-module.exports=R.m(96587).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xVGYaziAi2aqxTLzxK7MOD5Re2fbXZ6Jm8EcsNcLa9E=\"\n}"
